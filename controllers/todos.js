3c50b93bdf72cacfd098f02d4a8a399fa772d4ad
Henrik <henrik.saega@voco.ee>
2024-12-01 19:57:43 +0200

    Update the Todo by id

--- controllers/todos.js
+++ controllers/todos.js
@@ -12,7 +12,28 @@ class todoController{
 
         res.json({
             message: "Created new todo object",
-            newTask: newTodo
+            newTask: newTodo,
+        })
+    }
+
+    getTodos(req, res){
+        res.json({tasks: this.TODOS})
+    }
+
+    updateTodo(req, res){
+        const todoId = req.params.id
+        const updatedTask = req.body.task
+        const todoIndex = this.TODOS.findIndex((todo) => todo.id === todoId)
+        if(todoIndex < 0){
+            throw new Error("Couldn't find todo!")
+            res.json({
+                message: "Couln't find todo with such index!"
+            })
+        }
+        this.TODOS[todoIndex] = new Todo(this.TODOS[todoIndex].id, updatedTask)
+        res.json({
+            message: "Updated todo!",
+            updateTask: this.TODOS[todoIndex]
         })
     }
 }
