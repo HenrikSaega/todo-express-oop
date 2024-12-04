054729c13132902db08ecf3dc6c21a87adca34a2
Henrik <henrik.saega@voco.ee>
2024-12-04 20:09:32 +0200

    Read todos from file and write todos into file

--- controllers/todos.js
+++ controllers/todos.js
@@ -1,19 +1,30 @@
 import {Todo} from "../models/todo.js"
+import { fileManager } from "../files.js" 
 
 class todoController{
     constructor() {
         this.TODOS = []
     }
 
-    createTodo(req, res){
+    async createTodo(req, res){
         const task = req.body.task
         const newTodo = new Todo(Math.random().toString(), task)
         this.TODOS.push(newTodo)
-
+        await fileManager.writeFile("./data/todos.json", this.TODOS)
+        
         res.json({
             message: "Created new todo object",
             newTask: newTodo,
         })
+    }
+
+    async initTodos(){
+        const todosData = await fileManager.readFile("./data/todos.json")
+        if(todosData !== null){
+            this.TODOS = todosData
+        } else {
+            this.TODOS = []
+        }
     }
 
     getTodos(req, res){
---
+++ files.js
@@ -0,0 +1,25 @@
+import fs from "node:fs/promises"
+
+class FileManager{
+    async writeFile(filename, data) {
+        try {
+            data = JSON.stringify(data, null, 2)
+            await fs.writeFile(filename, data)
+        } catch (error) {
+            console.log("write error log => ", error)
+        }
+    }
+
+    async readFiles(filename){
+        try {
+            const fileContent = await fs.readFile(filename, "utf-8")
+            const fileData = JSON.parse(fileContent)
+            return fileData
+        } catch (error) {
+            console.log("read error log => ", error)
+            return null
+        }
+    }
+    
+}
+export const fileManager = new FileManager()
